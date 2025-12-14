b37e82eed52a379a27d205bc36fb0b305c1fcb62
Nastya <sergienkonasta350@gmail.com>
2025-12-14 19:52:42 +0300

    homework_12.2_done

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -6,13 +6,13 @@ from src.decorators import log
 
 
 # Пример успешной функции
-@log(filename="mylog.txt")
+@log(filename="../logs/mylog.txt")
 def successful_function(x: Any, y: Any) -> Any:
     return x + y
 
 
 # Пример функции, которая вызывает ошибку
-@log(filename="mylog.txt")
+@log(filename="../logs/mylog.txt")
 def error_function(x: Any, y: Any) -> Any:
     return x / y
 
@@ -21,7 +21,7 @@ def test_successful_function(capsys: Any) -> None:
     result = successful_function(1, 2)
     assert result == 3
 
-    with open("mylog.txt", "r") as log_file:
+    with open("../logs/mylog.txt", "r") as log_file:
         log_content = log_file.readlines()
         # Проверка, что лог содержит строку о запуске
         assert any("successful_function started with params" in line for line in log_content)
@@ -32,6 +32,6 @@ def test_successful_function(capsys: Any) -> None:
 def test_error_function(capsys: Any) -> None:
     with pytest.raises(ZeroDivisionError):
         error_function(1, 0)
-    with open("mylog.txt", "r") as log_file:
+    with open("../logs/mylog.txt", "r") as log_file:
         log_content = log_file.readlines()
         assert any("error_function failed with error: ZeroDivisionError" in line for line in log_content)
